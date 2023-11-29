ca038300c87698f24270ed4b040b6eb1ecefc9e4
SimoAO <aitouaazizmohammed@gmail.com>
2023-11-29 14:27:08 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,9 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+# Install a compatible version of Werkzeug
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
