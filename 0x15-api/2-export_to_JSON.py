11f2849262ae3e869a2c09ed69c85c3e65ee2368
simoao <aitouaazizmohammed@gmail.com>
2024-01-02 22:40:34 +0100

    task2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,21 +2,25 @@
 """ TO DO list information/progress module """
 import requests
 import sys
+import json
 
 
 if __name__ == '__main__':
-    """ CSV data exporting class """
+    """ JSON format data exporting class """
 
     u_id = sys.argv[1]
     url = 'https://jsonplaceholder.typicode.com/'
     user_url = url + 'users/{}'.format(u_id)
     info = requests.get(user_url).json()
     name = info.get('username')
+    u_json = {}
     todo_url = url + 'todos'
     todos = requests.get(todo_url).json()
+    dictt = {u_id: []}
 
-    with open('{}.csv'.format(u_id), 'w+') as file:
+    with open('{}.json'.format(u_id), 'w+') as file:
         for todo in todos:
-            file.write('"{}","{}","{}","{}"\n'
-                    .format(u_id, name, todo.get('completed'),
-                            todo.get('title')))
+            dictt[u_id].append({"task": todo.get('title'),
+                "completed": todo.get('completed'),
+                "username": name})
+            json.dump(dictt, file)
