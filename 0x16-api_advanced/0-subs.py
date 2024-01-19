e0381f2b0f378d3f2f8d08f4109b58b18ae22d42
simoao <aitouaazizmohammed@gmail.com>
2024-01-19 02:29:14 +0100

    task2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""number of subscribers module"""
+"""
+number of subscribers module
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """number of subscribers class"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    head = {"User-Agent": "MyReddit0"}
-    resp = requests.get(url, headers=head, allow_redirects=False)
+    url = "https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyReddit0"}
+    resp = requests.get(url, headers=headers, allow_redirects=False)
 
-    if resp.status_code != 400:
-        return resp.json().get("data").get("subscribers")
+    if resp.status_code == 200:
+        return resp.json().get('data').get('subscribers')
     else:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,10 +7,9 @@ import requests
 
 def top_ten(subreddit):
     """top ten class"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(
-            subreddit)
-    head = {"User-Agent": "MyReddit0"}
-    resp = requests.get(url, headers=head, allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "MyReddit0"}
+    resp = requests.get(url, headers=headers, allow_redirects=False)
 
     if resp.status_code == 200:
         for post in resp.json().get('data').get('children'):
