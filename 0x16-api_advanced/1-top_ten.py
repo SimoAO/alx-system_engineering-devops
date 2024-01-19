4494712bad4ada72c3c4059a3674c64e3c3de8b3
simoao <aitouaazizmohammed@gmail.com>
2024-01-19 02:43:14 +0100

    task3

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,9 +7,10 @@ import requests
 
 def top_ten(subreddit):
     """top ten class"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "MyReddit0"}
-    resp = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(
+            subreddit)
+    head = {"User-Agent": "MyReddit0"}
+    resp = requests.get(url, headers=head, allow_redirects=False)
 
     if resp.status_code == 200:
         for post in resp.json().get('data').get('children'):
