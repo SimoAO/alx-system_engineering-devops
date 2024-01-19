10e472113f2aecfa8d842d05ab2d614ad4df887d
simoao <aitouaazizmohammed@gmail.com>
2024-01-19 02:17:50 +0100

    task2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,6 +4,7 @@ recurse module
 """
 import requests
 
+
 def recurse(subreddit, hot_list=None, after=None):
     """recurse class"""
     if hot_list is None:
@@ -12,7 +13,9 @@ def recurse(subreddit, hot_list=None, after=None):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     head = {"User-Agent": "MyReddit0"}
     params = {"after": after}
-    resp = requests.get(url, headers=head, params=params, allow_redirects=False)
+    resp = requests.get(
+            url, headers=head, params=params, allow_redirects=False
+            )
 
     if resp.status_code == 200:
         after = resp.json().get('data').get('after')
