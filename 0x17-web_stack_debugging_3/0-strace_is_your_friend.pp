b86959390a2d45bdcb52d2d3f9abcc8fc9795b7a
simoao <aitouaazizmohammed@gmail.com>
2024-01-21 20:02:52 +0100

    task0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Automate puppet to fix apache error
 exec { 'fix wordpress':
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => "['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/']
+  path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
 }
